2710e4aee5fe4f40c1f44966bee3484f4dee1648
dxrahat33 <dxrahat33@gmail.com>
2024-09-23 21:08:13 +0600

    button configured

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-document.getElementById('btn-blog').addEventListener('click', function(){
+document.getElementById('btn-blog').addEventListener('click', function () {
     window.location.href = '/blog.html';
+})
+
+
+// Donation & History button 
+
+const btnDonation = getButtonInfo('btn-donation');
+const btnHistory = getButtonInfo('btn-history');
+btnDonation.addEventListener('click', function(){
+    btnDonation.classList.add('active');
+    btnHistory.classList.remove('active');
+})
+btnHistory.addEventListener('click', function(){
+    btnHistory.classList.add('active');
+    btnDonation.classList.remove('active');
 })
---
+++ js/utilities.js
@@ -0,0 +1,4 @@
+function getButtonInfo(id) {
+    const btn1 = document.getElementById(id);
+    return btn1;
+}
