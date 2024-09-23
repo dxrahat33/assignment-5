239bf3a8b1eea721011082709f616287fe6b7eb1
dxrahat33 <dxrahat33@gmail.com>
2024-09-23 22:49:00 +0600

    first donate form ready

--- js/script.js
+++ js/script.js
@@ -15,3 +15,25 @@ btnHistory.addEventListener('click', function(){
     btnHistory.classList.add('active');
     btnDonation.classList.remove('active');
 })
+
+// first donate form button function
+document.getElementById('btn-donate-1').addEventListener('click', function(event){
+    event.preventDefault();
+    const balance = getTextValue('account-balance');
+
+    const inputDonateNoakhali = getInputValue('input-donate-1');
+    document.getElementById('input-donate-1').value = ''
+
+    if(isNaN(inputDonateNoakhali) || inputDonateNoakhali > balance) {
+        alert('Invalid Number !')
+        return;
+    }
+
+    const newBalance = balance - inputDonateNoakhali;
+    const firstDonateBalance = getTextValue('first-donate-balance');
+
+    const newFirstDonateBalance = inputDonateNoakhali + firstDonateBalance;
+
+    document.getElementById('first-donate-balance').innerText = newFirstDonateBalance;
+    document.getElementById('account-balance').innerText = newBalance;
+});
--- js/utilities.js
+++ js/utilities.js
@@ -2,3 +2,15 @@ function getButtonInfo(id) {
     const btn1 = document.getElementById(id);
     return btn1;
 }
+
+function getInputValue(id) {
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+    return inputNumber;
+}
+
+function getTextValue(id) {
+    const textValue = document.getElementById(id).innerText;
+    const textValueNumber = parseFloat(textValue);
+    return textValueNumber;
+}
