9db18582e78c810199ec130fabf99d14ccf5dfe5
dxrahat33 <dxrahat33@gmail.com>
2024-09-25 10:42:22 +0600

    dark mode & light mode

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: false, // Disable dark mode
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
